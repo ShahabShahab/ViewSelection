053a459f17b9653c198219215a90bfdbc1603cfa
Shahab <shahabgolpaiegan@gmai.com>
2018-02-09 19:35:47 +0330

    First Commit!
    Right now you can enable singleselect or multiselect buttons over the layout and also specify the number of buttons through the xml file!

---
+++ app/src/main/java/tk/medlynk/shahab/MainActivity.java
@@ -0,0 +1,22 @@
+package tk.medlynk.shahab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tk.medlynk.shahab.viewselection.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewSelection buttons;
+    String text[] = {"shahab", "mammad", "shakib"}; 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_main );
+
+        buttons = findViewById ( R.id.buttons );
+        for (int i = 0 ; i < buttons.getNumberOfViews (); i++){
+            buttons.setTextToButtons ( text[i], i );
+        }
+    }
+}
