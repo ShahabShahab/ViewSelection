7a1e42e510077f8667da06c546f2c01a6a9158f0
shahabshahab <shahabgolpaiegan@gmail.com>
2018-03-01 11:03:32 +0330

    Callbacks return the view too!

--- app/src/main/java/com/medlynk/shahab/MainActivity.java
+++ app/src/main/java/com/medlynk/shahab/MainActivity.java
@@ -2,6 +2,7 @@ package com.medlynk.shahab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.medlynk.shahab.myviewselection.ViewSelection;
 import com.medlynk.shahab.viewselection.R;
@@ -11,9 +12,12 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements
         ViewSelection.OnSingleItemSelectedListener,
-        ViewSelection.OnMultiItemSelectedListener{
+        ViewSelection.OnMultiItemSelectedListener
+        {
 
     ViewSelection buttons;
+    ViewSelection buttons2;
+
     String text[] = {"shahab", "mammad", "shakib"};
     private List<Integer> selectedItems = new ArrayList<> (  );
     @Override
@@ -21,7 +25,9 @@ public class MainActivity extends AppCompatActivity implements
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
         buttons = findViewById ( R.id.buttons );
+        buttons2 = findViewById ( R.id.buttons2 );
         buttons.setOnSingleItemSelectedListener ( this );
+        buttons2.setOnSingleItemSelectedListener ( this );
         buttons.setOnMultiItemSelectedListener ( this );
         for (int i = 0 ; i < buttons.getNumberOfViews (); i++){
             buttons.setTextToButtons ( text[i], i );
@@ -29,13 +35,14 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
-    public void onSingleItemSelected(int position) {
+    public void onSingleItemSelected(View view, int position) {
         System.out.println ( "MainActivity.onSingleItemSelected" );
         System.out.println ( "position = [" + position + "]" );
+        System.out.println (view.getId ());
     }
 
     @Override
-    public void onMultiItemSelected(Integer position) {
+    public void onMultiItemSelected(View view, Integer position) {
         System.out.println ( "MainActivity.onMultiItemSelected" );
         System.out.println ( "position = [" + position + "]" );
         selectedItems.add ( position );
@@ -45,7 +52,7 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
-    public void onMultiItemDeselected(Integer position) {
+    public void onMultiItemDeselected(View view, Integer position) {
         System.out.println ( "MainActivity.onMultiItemDeselected" );
         selectedItems.remove ( position );
         for (Integer integer: selectedItems){
--- myviewselection/src/main/java/com/medlynk/shahab/myviewselection/ViewSelection.java
+++ myviewselection/src/main/java/com/medlynk/shahab/myviewselection/ViewSelection.java
@@ -20,8 +20,8 @@ import java.util.List;
 
 public class ViewSelection extends LinearLayout {
 
-    List<Button> buttons = new ArrayList<> (  );
-    List<EditText> editTexts = new ArrayList<> (  );
+    List<Button> buttons = new ArrayList<> ();
+    List<EditText> editTexts = new ArrayList<> ();
     int numOfViews = 0;
     Boolean selectable = false;
     private int currentSelection = -1;
@@ -63,6 +63,7 @@ public class ViewSelection extends LinearLayout {
     public void setSelectable(Boolean selectable) {
         this.selectable = selectable;
     }
+
     public OnMultiItemSelectedListener getOnMultiItemSelectedListener() {
         return onMultiItemSelectedListener;
     }
@@ -87,7 +88,7 @@ public class ViewSelection extends LinearLayout {
         this.buttons = buttons;
     }
 
-    public List<EditText> getEditTexts(){
+    public List<EditText> getEditTexts() {
         return editTexts;
     }
 
@@ -131,46 +132,46 @@ public class ViewSelection extends LinearLayout {
         this.unselected_state_background = unselected_state_background;
     }
 
-    private OnClickListener button_click_listener = new OnClickListener (){
+    private OnClickListener button_click_listener = new OnClickListener () {
         @Override
         public void onClick(View view) {
             currentSelection = view.getId ();
             if (selectable) {
-                if( single_select ){
-                    if( onSingleItemSelectedListener == null ){
+                if (single_select) {
+                    if (onSingleItemSelectedListener == null) {
                         throw new RuntimeException ( getmContext ().toString () + "must implement " + OnSingleItemSelectedListener.class.getSimpleName () );
-                    }else
-                        onSingleItemSelectedListener.onSingleItemSelected ( currentSelection );
+                    } else
+                        onSingleItemSelectedListener.onSingleItemSelected ( ViewSelection.this, currentSelection );
                     for (Button button : buttons) {
-                        if( button.getId () == currentSelection){
+                        if (button.getId () == currentSelection) {
                             button.setTextColor ( selected_text_color );
                             button.setBackground ( selected_state_drawable );
-                        }else{
+                        } else {
                             button.setBackground ( unselected_state_drawbale );
                             button.setTextColor ( unselected_text_color );
                         }
                     }
-                }else{
-                    if( buttons.get ( currentSelection ).getBackground ().equals ( selected_state_drawable ) ) {
+                } else {
+                    if (buttons.get ( currentSelection ).getBackground ().equals ( selected_state_drawable )) {
                         buttons.get ( currentSelection ).setBackground ( unselected_state_drawbale );
-                        buttons.get ( currentSelection ).setTextColor (unselected_text_color);
-                        if( onMultiItemSelectedListener == null ){
+                        buttons.get ( currentSelection ).setTextColor ( unselected_text_color );
+                        if (onMultiItemSelectedListener == null) {
                             throw new RuntimeException ( getmContext ().toString () + "must implement " + OnMultiItemSelectedListener.class.getSimpleName () );
-                        }else{
-                            onMultiItemSelectedListener.onMultiItemDeselected ( currentSelection );
+                        } else {
+                            onMultiItemSelectedListener.onMultiItemDeselected (ViewSelection.this,  currentSelection );
                         }
-                    }else{
-                        if( onMultiItemSelectedListener == null ){
+                    } else {
+                        if (onMultiItemSelectedListener == null) {
                             throw new RuntimeException ( getmContext ().toString () + " must implement " + OnMultiItemSelectedListener.class.getSimpleName () );
-                        }else{
-                            onMultiItemSelectedListener.onMultiItemSelected ( currentSelection );
+                        } else {
+                            onMultiItemSelectedListener.onMultiItemSelected ( ViewSelection.this,  currentSelection );
                         }
                         buttons.get ( currentSelection ).setBackground ( selected_state_drawable );
                         buttons.get ( currentSelection ).setTextColor ( selected_text_color );
                     }
                 }
-            }else{
-                for (Button button: buttons){
+            } else {
+                for (Button button : buttons) {
                     button.setBackground ( unselectable_drawable );
                     button.setTextColor ( unselectable_text_color );
                 }
@@ -185,7 +186,7 @@ public class ViewSelection extends LinearLayout {
     public ViewSelection(Context context, AttributeSet attrs) {
         super ( context, attrs );
         this.mContext = context;
-        makeView (context, attrs);
+        makeView ( context, attrs );
     }
 
     public ViewSelection(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -198,7 +199,7 @@ public class ViewSelection extends LinearLayout {
         View view = LayoutInflater.from ( context ).inflate ( R.layout.viewselection_parent_view, this, true );
         LinearLayout linearLayout = view.findViewById ( R.id.parent );
         TypedArray typedArray = context.
-                obtainStyledAttributes(attrs, R.styleable.ViewSelection, 0, 0);
+                obtainStyledAttributes ( attrs, R.styleable.ViewSelection, 0, 0 );
         selected_state_background = typedArray.getResourceId ( R.styleable.ViewSelection_selected_background, R.drawable.selected_stated );
         selected_state_drawable = context.getResources ().getDrawable ( selected_state_background );
         unselected_state_background = typedArray.getResourceId ( R.styleable.ViewSelection_unselected_background, R.drawable.unselected_state );
@@ -206,51 +207,48 @@ public class ViewSelection extends LinearLayout {
         unselectable_background = typedArray.getResourceId ( R.styleable.ViewSelection_unselectable_background, R.drawable.unselected_state );
         unselectable_drawable = context.getResources ().getDrawable ( unselectable_background );
         selected_text_color = typedArray.getInt ( R.styleable.ViewSelection_selected_text_color, R.color.selected_text_clolor );
-        unselected_text_color = typedArray.getInt ( R.styleable.ViewSelection_unselected_text_color, R.color.unselected_text_color ) ;
-        unselectable_text_color = typedArray.getInt ( R.styleable.ViewSelection_unselectable_text_color,  R.color.unselectable_text_color );
+        unselected_text_color = typedArray.getInt ( R.styleable.ViewSelection_unselected_text_color, R.color.unselected_text_color );
+        unselectable_text_color = typedArray.getInt ( R.styleable.ViewSelection_unselectable_text_color, R.color.unselectable_text_color );
         selectable = typedArray.getBoolean ( R.styleable.ViewSelection_selectable, false );
         single_select = typedArray.getBoolean ( R.styleable.ViewSelection_single_select, false );
         numOfViews = typedArray.getInt ( R.styleable.ViewSelection_number_of_views, 1 );
-        button_type= typedArray.getBoolean ( R.styleable.ViewSelection_button_type, false );
+        button_type = typedArray.getBoolean ( R.styleable.ViewSelection_button_type, false );
         edittext_type = typedArray.getBoolean ( R.styleable.ViewSelection_edittext_type, false );
-        if( !typedArray.hasValue ( R.styleable.ViewSelection_button_type ) &&
-               !typedArray.hasValue ( R.styleable.ViewSelection_edittext_type )){
-                throw new RuntimeException ( ViewSelection.class.getSimpleName () + " must have a view type!" );
-        }else if ( button_type && edittext_type ) {
+        if (!typedArray.hasValue ( R.styleable.ViewSelection_button_type ) &&
+                !typedArray.hasValue ( R.styleable.ViewSelection_edittext_type )) {
+            throw new RuntimeException ( ViewSelection.class.getSimpleName () + " must have a view type!" );
+        } else if (button_type && edittext_type) {
             throw new RuntimeException ( ViewSelection.class.getSimpleName () + " can only have a single view type\nCurrently has both button type and EditText type!" );
-        }else
-                if( button_type ){
-                    for( int i = 0 ; i < numOfViews ; i++){
-                        Button button = new Button ( context );
-                        button.setPadding ( 10, 10,10,10 );
-                        if( selectable ){
-                            button.setBackgroundResource ( unselected_state_background );
-                            button.setTextColor ( unselected_text_color );
-                        }
-                        else{
-                            button.setBackgroundResource ( unselectable_background );
-                            button.setTextColor ( unselectable_text_color );
-                        }
-                        LayoutParams layoutParams = new LayoutParams ( ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT );
-                        layoutParams.setMargins ( 10, 10, 10, 10 );
-                        button.setLayoutParams ( layoutParams );
-                        button.setId ( i );
-                        button.setOnClickListener ( button_click_listener );
-                        buttons.add ( button );
-                        linearLayout.addView ( button );
-                    }
-                }else if ( edittext_type ) {
-                    for (int i = 0; i < numOfViews; i++) {
-                        EditText editText = new EditText ( context );
-                        editText.setPadding ( 10, 10 ,10, 10 );
-                        LayoutParams layoutParams = new LayoutParams ( ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT );
-                        layoutParams.setMargins ( 10, 10, 10, 10 );
-                        editText.setLayoutParams ( layoutParams );
-                        editTexts.add ( editText );
-                        linearLayout.addView ( editText );
-                    }
+        } else if (button_type)
+            for (int i = 0; i < numOfViews; i++) {
+                Button button = new Button ( context );
+                button.setPadding ( 10, 10, 10, 10 );
+                if (selectable) {
+                    button.setBackgroundResource ( unselected_state_background );
+                    button.setTextColor ( unselected_text_color );
+                } else {
+                    button.setBackgroundResource ( unselectable_background );
+                    button.setTextColor ( unselectable_text_color );
                 }
-            }
+                LayoutParams layoutParams = new LayoutParams ( ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT );
+                layoutParams.setMargins ( 10, 10, 10, 10 );
+                button.setLayoutParams ( layoutParams );
+                button.setId ( i );
+                button.setOnClickListener ( button_click_listener );
+                buttons.add ( button );
+                linearLayout.addView ( button );
+            } else if(edittext_type ){
+        for (int i = 0; i < numOfViews; i++) {
+            EditText editText = new EditText ( context );
+            editText.setPadding ( 10, 10, 10, 10 );
+            LayoutParams layoutParams = new LayoutParams ( ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT );
+            layoutParams.setMargins ( 10, 10, 10, 10 );
+            editText.setLayoutParams ( layoutParams );
+            editTexts.add ( editText );
+            linearLayout.addView ( editText );
+        }
+    }
+}
     public void setTextToButtons( String text, int position ){
         buttons.get ( position ).setText ( text );
     }
@@ -265,10 +263,10 @@ public class ViewSelection extends LinearLayout {
     }
 
     public interface OnSingleItemSelectedListener {
-        void onSingleItemSelected(int position );
+        void onSingleItemSelected(View view, int position );
     }
     public interface OnMultiItemSelectedListener{
-        void onMultiItemSelected(Integer position);
-        void onMultiItemDeselected(Integer position);
+        void onMultiItemSelected(View view, Integer position);
+        void onMultiItemDeselected( View view,  Integer position);
     }
 }
